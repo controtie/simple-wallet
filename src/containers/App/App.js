fb9148f26eff815eaf5b151f1cb8b6295594a9d2
controtie <dylan.ad.tran@gmail.com>
2017-08-19 17:20:26 -0700

    setup redux store with thunk middleware

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
-import logo from '../../logo.svg';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import rootReducer from '../../root-reducer';
 import '../../styles/App.css';
+
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+const store = createStoreWithMiddleware(rootReducer);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Provider store={store}>
+        <div className="App">
+          <p> sup guys! </p>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Provider>
     );
   }
 }
 
 export default App;
+
--- src/root-reducer.js
+++ src/root-reducer.js
@@ -1,9 +1,9 @@
-import { createStore } from 'redux';
+import { combineReducers } from 'redux';
 
 // This root reducer is the base of the application state tree:
 // SCHEMA GOES HERE
 
-export default {
-  state: true,
-};
+export default combineReducers({
+
+});
 
