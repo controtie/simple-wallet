42eb2034943901435a9e43f9570155b5e9659049
controtie <dylan.ad.tran@gmail.com>
2017-08-23 14:39:18 -0700

    integrate react-router-redux

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
 import React, { Component } from 'react';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
+import createHistory from 'history/createBrowserHistory';
 import thunk from 'redux-thunk';
+import { ConnectedRouter, routerMiddleware } from 'react-router-redux';
+import { Provider } from 'react-redux';
+import { Route } from 'react-router';
+import { createStore, applyMiddleware } from 'redux';
+
 import rootReducer from './root-reducer';
 import './styles/App.css';
 import RootComponent from './components/RootComponent';
 
-// 1. Configure redux store to accept 'redux-thunk' middleware
-// 2. createStore with root-reducer as application state tree
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+// Create new browser history, and allow it to intercept changes to redux store as middleware
+// Apply thunks to redux, allowing delayed calling of 'dispatch'
+const history = createHistory();
+const historyMiddleware = routerMiddleware(history);
+const createStoreWithMiddleware = applyMiddleware(thunk, historyMiddleware)(createStore);
 const store = createStoreWithMiddleware(rootReducer);
 
 // This is the root component of the application to be mounted on the DOM.
 // The component is wrapped with a Provider that has access to our application store
 // All subcomponents have access to the store with react-redux 'connect' method
-// With 'connect' each subcomponent can listen directly to the store,
-// no passing of props required
+// They also have access to browser history with react-router-redux 'push' method
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
         <div className="App">
-          <RootComponent />
+          <ConnectedRouter history={history}>
+            <Route exact path="/" component={RootComponent} />
+          </ConnectedRouter>
         </div>
       </Provider>
     );
--- src/root-reducer.js
+++ src/root-reducer.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import balance from './containers/Balance/balance-reducer';
 import login from './containers/Login/login-reducer';
+import { routerReducer as router } from 'react-router-redux'; // allows us to track/modify browser history by updating redux state
 
 // This root reducer is the base of the application state tree:
 // SCHEMA GOES HERE
@@ -8,5 +9,6 @@ import login from './containers/Login/login-reducer';
 export default combineReducers({
   balance,
   login,
+  router,
 });
 
